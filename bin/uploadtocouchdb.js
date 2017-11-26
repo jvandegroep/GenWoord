b88c5a07e6b2407d6433dc688c792d4e537bc70e
Jacob van de Groep <jaapgroep@hotmail.com>
2017-11-26 23:19:52 +0100

    fixing put request

--- bin/uploadtocouchdb.js
+++ bin/uploadtocouchdb.js
@@ -4,21 +4,22 @@
 //
 
 var fs = require('fs');
+var Combinatorics = require('js-combinatorics');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-//create web request
-function httpData(url,cmd,postData,res){
+// Get DB data from url and send back data
+function httpData(url,putData,res){
    var xhttp = new XMLHttpRequest();
+
    xhttp.onreadystatechange = function() {
      if (xhttp.readyState == 4 && (xhttp.status == 200 || xhttp.status == 201)) {
-          //console.log("data received (200) from: " + DBHOST + " on port: " + DBPORT);
+          console.log("data received (200) from: ", url);
           res(xhttp.responseText);
         }
     if (xhttp.readyState == 4 && xhttp.status == 404) {
       console.log("connection failed, no response from URL:", url);
     }
   };
-  xhttp.open(cmd, url, true);
   xhttp.timeout = 2000; // time in milliseconds
   xhttp.ontimeout = function(e) {
     console.error("Timeout, cannot contact ", url);
@@ -28,10 +29,14 @@ function httpData(url,cmd,postData,res){
     console.log("** An error occurred during the transaction");
     res("");
   };
-  if (postData) {
+
+  if (putData) {
+    xhttp.open("PUT", url, "/json-handler");
     xhttp.setRequestHeader('Content-type','application/json; charset=utf-8');
-    xhttp.send(postData);
+    xhttp.send(putData);
+    console.log('response from server: ',xhttp.responseText);
   } else {
+    xhttp.open("GET", url, true);
     xhttp.send();
   }
 }
@@ -42,13 +47,68 @@ var DBPORT = "5984";
 var DBNAME = "genwoorddb";
 var DESIGNNAME = "wordindexdutch";
 var DBURL = "http://" + DBHOST + ":" + DBPORT + "/" + DBNAME + "/_design/" + DESIGNNAME + "/_view/" + DESIGNNAME;
-var dicFile = './dics/dutch/OpenTaal-210G-basis-gekeurd.txt';
-// http://192.168.178.2:5984/genwoorddb/_design/wordindexdutch/_view/wordindexdutch?key=%22abl%22&include_docs=true
+var DBURLSHORT = "http://" + DBHOST + ":" + DBPORT + "/" + DBNAME + "/";
+var DBURLUUIDS = "http://" + DBHOST + ":" + DBPORT + "/_uuids";
 
-var testURL = DBURL + "?key=%22abl%22&include_docs=true";
-// Connect to genwoorddb
-httpData(testURL,"GET", "", function(res){
+// dutch dictionary file
+var dicFile = './bin/dics/dutch/OpenTaal-210G-basis-gekeurd.txt';
 
-  console.log("DBURL = ", DBURL);
-  console.log("ResultJSON = ",(JSON.parse(res)).rows);
+//load dictionary file
+fs.readFile(dicFile, "utf8", function(err, data){
+  if(err) {
+    console.log('error loading dictonary file', dicFile + "error:",err);
+    return false;
+  }
+
+  console.log('File',dicFile + ' loaded!');
+
+  // convert data to array
+  var dicTextFile = data.split(/\r\n|\n/);
+
+  // do for each word in dicTextfile
+  dicTextFile.forEach(function(word){
+
+    //split, sort word
+    var sortedWord = word.split("").sort().join("");
+
+    var swURL = DBURL + "?key=%22" + sortedWord + "%22&include_docs=true";
+
+    // Connect to genwoorddb
+    httpData(swURL, "", function(res){
+
+      res = JSON.parse(res);
+
+      //if document does not exist
+      if (res.rows.length < 1){
+        console.log('document with key',word + ' not found, creating a new doc');
+
+        httpData(DBURLUUIDS, "", function(res2){
+
+          var UUIDS = JSON.parse(res2).uuids;
+          var newData = '{ "sortedword" : "' + sortedWord + '", "word" : "' + word + '", "language" : "dutch" }';
+
+          var DBURLPUT = DBURLSHORT + UUIDS;
+
+          httpData(DBURLPUT, newData, function(res3){
+
+            if (err) {
+              console.log('error creating doc, URL: ', createNewDocURL + 'data: ',newData);
+              return;
+            }
+            console.log('document \"',word + "\" was created.");
+            console.log('response from URL: ',DBURLPUT, res3 );
+          });
+        });
+
+
+      } else {
+        // document does exist
+        console.log('document \"',word + "\" does already exist.");
+
+      }
+    });
+
+
+  });
+
 });
